e19ac203825ee22ae988537e32750e47029f8eb6
Cory Harper <cory@hoa-express.com>
2021-02-11 19:50:23 -0600

    added tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,33 @@
-import helloWorld from '../src/index';
+// Imports
+import cssValidator from '../src';
 
-describe('#helloWorld()', () => {
-	it('Works', () => {
-		expect(helloWorld()).toEqual('Hello world!');
+// Tests
+describe('#validateText()', () => {
+	it('Returns the validity and errors when no options are provided', async () => {
+		expect(await cssValidator.validateText('.foo { text-align: center; }')).toStrictEqual({
+			valid: true,
+			errors: [],
+		});
+	});
+
+	it('Returns the validity, errors, and warnings when a warning level option is provided', async () => {
+		expect(await cssValidator.validateText('.foo { text-align: center; }', { warningLevel: 1 })).toStrictEqual({
+			valid: true,
+			errors: [],
+			warnings: [],
+		});
+	});
+
+	it('Includes errors present in the response on the result', async () => {
+		expect(await cssValidator.validateText('.foo { text-align: center; ')).toStrictEqual({
+			valid: false,
+			errors: [
+				{
+					line: 1,
+					// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+					message: expect.any(String),
+				},
+			],
+		});
 	});
 });
