80c11c88f17b2d30f6d257235a683cd10ddf46c6
Cory Harper <cory@hoa-express.com>
2021-02-18 15:19:54 -0600

    disabled linter for symlinked package during testing

--- test/puppeteer/src/index.ts
+++ test/puppeteer/src/index.ts
@@ -2,5 +2,6 @@
 import helloWorld from 'w3c-css-validator';
 
 document.addEventListener('DOMContentLoaded', () => {
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
 	document.title = helloWorld();
 });
