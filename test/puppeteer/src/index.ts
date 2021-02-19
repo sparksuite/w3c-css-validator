da1ce38cb901d77574f4a68984f01e72b659b6ba
Cory Harper <cory@hoa-express.com>
2021-02-19 13:12:48 -0600

    Added comment where appropriate

--- test/puppeteer/src/index.ts
+++ test/puppeteer/src/index.ts
@@ -1,6 +1,7 @@
 // Imports
 import helloWorld from 'w3c-css-validator';
 
+// Wait until DOM content is available to attempt to make changes
 document.addEventListener('DOMContentLoaded', () => {
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
 	document.title = helloWorld();
