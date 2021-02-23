7413180cebf8d53cc1a23df6af6b8f3a47082b3d
Cory Harper <cory@hoa-express.com>
2021-02-23 16:36:18 -0600

    moved type and corrected imports

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,6 @@
 // Imports
 import validateText from './validate-text';
 
-// Define types
-export interface W3CCSSValidatorResponse {
-	cssvalidation: {
-		validity: boolean;
-		errors?: {
-			line: number;
-			message: string;
-		}[];
-		warnings?: {
-			line: number;
-			level: 0 | 1 | 2;
-			message: string;
-		}[];
-	};
-}
-
 // Validates CSS using W3C's public CSS validator service
 const cssValidator = {
 	validateText,
--- src/retrieve-w3c-validation/index.ts
+++ src/retrieve-w3c-validation/index.ts
@@ -1,7 +1,22 @@
 // Imports
-import { W3CCSSValidatorResponse } from '..';
 import retrieveForBrowser from './browser';
 import retrieveForNode from './node';
+
+// Define types
+export interface W3CCSSValidatorResponse {
+	cssvalidation: {
+		validity: boolean;
+		errors?: {
+			line: number;
+			message: string;
+		}[];
+		warnings?: {
+			line: number;
+			level: 0 | 1 | 2;
+			message: string;
+		}[];
+	};
+}
 
 // Function that detects the appropriate HTTP request client and returns a response accordingly
 const retrieveW3CValidation = async (url: string): Promise<W3CCSSValidatorResponse['cssvalidation']> => {
