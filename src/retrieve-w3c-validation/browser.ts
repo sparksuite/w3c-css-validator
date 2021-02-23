e3116dd12ead707774e1abb3653677526ae71b21
Cory Harper <cory@hoa-express.com>
2021-02-23 11:37:42 -0600

    created more code splitting for environments

---
+++ src/retrieve-w3c-validation/browser.ts
@@ -0,0 +1,12 @@
+// Imports
+import { W3CCSSValidatorResponse } from '../types';
+
+// Utility function for retrieving response from W3C CSS Validator in a browser environment
+const retrieveForBrowser = async (url: string): Promise<W3CCSSValidatorResponse['cssvalidation']> => {
+	const res = await fetch(url);
+	const data = (await res.json()) as W3CCSSValidatorResponse;
+
+	return data.cssvalidation;
+};
+
+export default retrieveForBrowser;
--- src/retrieve-w3c-validation/index.ts
+++ src/retrieve-w3c-validation/index.ts
@@ -1,14 +1,12 @@
 // Imports
 import { W3CCSSValidatorResponse } from '../types';
-import retrieveForNode from './retrieveForNode';
+import retrieveForBrowser from './browser';
+import retrieveForNode from './node';
 
 // Function that detects the appropriate HTTP request client and returns a response accordingly
 const retrieveW3CValidation = async (url: string): Promise<W3CCSSValidatorResponse['cssvalidation']> => {
 	if (typeof window?.fetch === 'function') {
-		const res = await fetch(url);
-		const data = (await res.json()) as W3CCSSValidatorResponse;
-
-		return data.cssvalidation;
+		return await retrieveForBrowser(url);
 	}
 
 	return await retrieveForNode(url);
