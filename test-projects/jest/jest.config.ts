ac884220edbb96770df0a3bcbfe8601d20219fad
Wes Cossick <wes@sparksuite.com>
2021-03-01 13:21:05 -0600

    Fix coverage reporting

--- test-projects/jest/jest.config.ts
+++ test-projects/jest/jest.config.ts
@@ -1,9 +1,6 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-	collectCoverage: true,
-	coverageDirectory: './coverage/',
-	collectCoverageFrom: ['<rootDir>/src/**'],
 	displayName: 'Full test suite',
 	verbose: true,
 	preset: 'ts-jest',
