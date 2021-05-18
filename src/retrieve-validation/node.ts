c9114ba29c86c6c8dff67eb81e7b9bb7f59cd9d7
Cory Harper <zepher2211@gmail.com>
2021-05-18 11:49:40 -0500

    added timeout functionality to node

--- src/retrieve-validation/node.ts
+++ src/retrieve-validation/node.ts
@@ -3,18 +3,32 @@ import * as https from 'https';
 import { W3CCSSValidatorResponse } from '.';
 
 // Utility function for retrieving response from W3C CSS Validator in a Node.js environment
-const retrieveInNode = async (url: string): Promise<W3CCSSValidatorResponse['cssvalidation']> => {
-	return new Promise((resolve) => {
-		https.get(url, (res) => {
-			let data = '';
+const retrieveInNode = async (url: string, timeout?: number): Promise<W3CCSSValidatorResponse['cssvalidation']> => {
+	return new Promise((resolve, reject) => {
+		const req = https.get(
+			url,
+			{
+				timeout,
+			},
+			(res) => {
+				let data = '';
 
-			res.on('data', (chunk) => {
-				data += chunk;
-			});
+				res.on('data', (chunk) => {
+					data += chunk;
+				});
 
-			res.on('end', () => {
-				resolve((JSON.parse(data) as W3CCSSValidatorResponse).cssvalidation);
-			});
+				res.on('end', () => {
+					resolve((JSON.parse(data) as W3CCSSValidatorResponse).cssvalidation);
+				});
+			}
+		);
+
+		req.on('timeout', () => {
+			reject(
+				new Error(
+					`The request took longer than ${timeout !== undefined ? `${timeout}ms` : "the browser's default timeout"}`
+				)
+			);
 		});
 	});
 };
