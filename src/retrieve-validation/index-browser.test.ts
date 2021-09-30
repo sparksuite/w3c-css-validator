15d47a889b25f56ac2c0f2f4fd89bbabeb12438c
Cory Harper <zepher2211@gmail.com>
2021-09-30 16:42:00 -0500

    introduced retrieveValidation tests

---
+++ src/retrieve-validation/index-browser.test.ts
@@ -0,0 +1,25 @@
+/**
+ * @jest-environment jsdom
+ */
+
+// Imports
+import retrieveValidation from '.';
+import retrieveInBrowser from './browser';
+import 'whatwg-fetch';
+
+// Mocks
+jest.mock('./browser');
+
+// Tests
+describe('#retrieveValidation()', () => {
+	it('Uses retrieveInBrowser() when the Fetch API is available', async () => {
+		require('whatwg-fetch');
+
+		await retrieveValidation(
+			'https://jigsaw.w3.org/css-validator/validator?text=.foo%20%7B%20text-align%3A%20center%3B%20%7D&usermedium=all&warning=no&output=application/json&profile=css3',
+			3000
+		);
+
+		expect(retrieveInBrowser as jest.Mock).toBeCalled();
+	});
+});
