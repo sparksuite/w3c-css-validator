86cfe62902aa509113da6475474d9e0d7a47b636
Cory Harper <zepher2211@gmail.com>
2022-06-17 18:59:56 -0500

    formatting

--- src/retrieve-validation/process-parameters.test.ts
+++ src/retrieve-validation/process-parameters.test.ts
@@ -1,15 +1,15 @@
 // Imports
-import buildRequestURLParameters from "./build-request-url-parameters";
-import processParameters from "./process-parameters";
+import buildRequestURLParameters from './build-request-url-parameters';
+import processParameters from './process-parameters';
 
 // Tests
 describe('#processParameters()', () => {
 	it('Returns URL parameters for GET requests', () => {
-        const parameters = {
-            text: '.foo { text-align: center; }',
-            medium: undefined,
-            warningLevel: undefined,
-        };
+		const parameters = {
+			text: '.foo { text-align: center; }',
+			medium: undefined,
+			warningLevel: undefined,
+		};
 
 		expect(processParameters('GET', parameters)).toBe(buildRequestURLParameters(parameters));
 	});
