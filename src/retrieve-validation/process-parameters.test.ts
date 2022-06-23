16d453d5a14aa5f9d2259f281ed9092a5d0de71c
Cory Harper <zepher2211@gmail.com>
2022-06-23 12:56:38 -0500

    added tests

--- src/retrieve-validation/process-parameters.test.ts
+++ src/retrieve-validation/process-parameters.test.ts
@@ -1,4 +1,5 @@
 // Imports
+import buildFormData from './build-form-data';
 import buildRequestURLParameters from './build-request-url-parameters';
 import processParameters from './process-parameters';
 
@@ -14,6 +15,16 @@ describe('#processParameters()', () => {
 		expect(processParameters('GET', parameters)).toBe(buildRequestURLParameters(parameters));
 	});
 
+	it('Returns form data for POST requests', () => {
+		const parameters = {
+			text: '.foo { text-align: center; }',
+			medium: undefined,
+			warningLevel: undefined,
+		};
+
+		expect(processParameters('POST', parameters)).toBe(buildFormData(parameters));
+	});
+
 	it('Throws an error if an unrecognized method is used', () => {
 		const parameters = {
 			text: '.foo { text-align: center; }',
@@ -24,6 +35,30 @@ describe('#processParameters()', () => {
 		// @ts-expect-error We are trying to throw an error here
 		expect(() => processParameters('PATCH', parameters)).toThrow(
 			'Parameter processing called with unrecognized method: PATCH'
+		);
+	});
+
+	it('Throws an error if a GET request is made with a `text` parameter', () => {
+		const parameters = {
+			text: '.foo { text-align: center; }',
+			medium: undefined,
+			warningLevel: undefined,
+		};
+
+		expect(() => processParameters('GET', parameters)).toThrow(
+			'A GET request is not supported with validation by text'
+		);
+	});
+
+	it('Throws an error if a POST request is made with a `url` parameter', () => {
+		const parameters = {
+			url: 'https://raw.githubusercontent.com/sparksuite/w3c-css-validator/master/public/css/valid.css',
+			medium: undefined,
+			warningLevel: undefined,
+		};
+
+		expect(() => processParameters('POST', parameters)).toThrow(
+			'A POST request is not supported with validation by URL'
 		);
 	});
 });
